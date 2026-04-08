77cf96849e288f1a67a429547bf1711983cba1db
Zackkkkun <wmzazzikry1994@gmail.com>
2026-04-08 19:14:13 +0800

    connecting frontend and backend

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask is working!"
+    return render_template("index.html")
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    data = request.json
+    user_answer = data.get("answer")
+
+    return jsonify({
+        "message": f"You said: {user_answer}"
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
